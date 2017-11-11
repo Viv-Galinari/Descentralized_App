9c598ad51daaaf5e1c8631abbcbb49767ba2eb0d
Viv <vivian_pr@hotmail.com>
2017-11-11 21:50:42 +0000

    added SEO friendly urls and new fields

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,17 @@ class Review < ApplicationRecord
 
 validates :title, presence: true
 validates :body, length: {minimum: 10}
-validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 0 }
+validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+validates :restaurant, presence: true
 
+# To change default URL pattern to SEO friendly
+# Take the Id and turn them into string
+# Add a dash, and turn title into URL
+# the dash will remove the sapces (%20)
+
+def to_param
+ id.to_s + "-" + title.parameterize
+end
 
 
 end
