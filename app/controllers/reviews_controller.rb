03894a9dc7512dda423ab9039d5e88982792affa
Viv <vivian_pr@hotmail.com>
2017-11-13 22:33:49 +0000

    added geolocation to site

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,42 +1,47 @@
 class ReviewsController < ApplicationController
 
   # Index is our list page for our reviews
-  # We only want a list of all of the reviews if there is no filter
   def index
 
     # We create filters so user can filter content
-    # Filters are created by using params(such as price filter)
+    # Filters are created by using variables and params(such as price filter)
     @price = params[:price]
     @cuisine = params[:cuisine]
+    @location = params[:location]
 
-    # start with all reviews
+    # start with all reviews from the Model (data)
     @reviews = Review.all
 
     # Filter by price
     # Run if statement to check if there is price
       if @price.present?
-        # Set variable to show all reviews that have certain price to it
-        # if there is price, get all of these reviews (@reviews = Review.all)
+        # if there is price, set a new variable to get all of these reviews (@reviews = Review.all)
         # And replace to reviews where there is a price
-          @reviews = reviews.where(price: @price)
+          @reviews = @reviews.where(price: @price)
+        end
 
         # Filter by cuisine
-        if cuisine.present?
-          @reviews = reviews.where(cuisine: @cuisine)
+        if @cuisine.present?
+          @reviews = @reviews.where(cuisine: @cuisine)
         end
+
+        # search near the location
+       if @location.present?
+         @reviews = @reviews.near(@location)
+       end
 
   end
 
   # This is the form for adding a new review
   # define a new page
   def new
-    @review = Review.new
-  end
+     @review = Review.new
+   end
 
   # To create a new review
   # Take the information from the form and add to the database
   # this will create a new row in the database
-  # Use params to require reviews, allowing users to only add 3 things
+  # Use params to require reviews, allowing users to only add what's required
   # Symblos are variables that dont change
   def create
     @review = Review.new(form_params)
@@ -44,7 +49,7 @@ class ReviewsController < ApplicationController
     # we want to check if the model can be saved
     # if can be saved, go to homepage again
     # with every if statement there is an end
-    #  if can't be saved, show the view for new.html.erb
+    # if can't be saved, show the view for new.html.erb
     if @review.save
       redirect_to root_path
     else
@@ -55,7 +60,7 @@ class ReviewsController < ApplicationController
 
   # To show an individual review page
   def show
-    @review= Review.find(params[:id])
+    @review = Review.find(params[:id])
   end
 
 # To delete a review
@@ -69,27 +74,28 @@ class ReviewsController < ApplicationController
   end
 
 # To find an individual review and to edit it
-  def edit
-    @review = Review.find(params[:id])
-  end
+def edit
+  @review = Review.find(params[:id])
+end
 
-  # To update review a review
+  # To update a review
   # find the individual review
   # update database with the new info from the form
   # redirect somewhere new
   # If there is any validation error when inputing the review
   # redirect user to edit page
   def update
-    @review = Review.find(params[:id])
-       if @review.update(form_params)
-      redirect_to review_path(@review)
-    else
-      render "edit"
-    end
-  end
+     @review = Review.find(params[:id])
+     if @review.update(form_params)
+       redirect_to review_path(@review)
+     else
+       render "edit"
+     end
+   end
 
   def form_params
-    params.require(:review).permit(:title,:restaurant, :ambiance, :body, :score, :price, :cuisine)
-  end
+     params.require(:review).permit(:title, :restaurant, :body, :score,
+       :ambiance, :cuisine, :price, :address)
+   end
 
-end
+ end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111223627) do
+ActiveRecord::Schema.define(version: 20171112115205) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20171111223627) do
     t.string "ambiance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
