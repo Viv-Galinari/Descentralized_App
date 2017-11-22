7c36cd318c27960adcff04d6d43e8268e45637d5
Viv <vivian_pr@hotmail.com>
2017-11-22 23:31:10 +0000

    added photo uploader and admin user

--- Desktop/sites/bien/config/routes.rb
+++ Desktop/sites/bien/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  ActiveAdmin.routes(self)
 # resources set up all URLS to be used for controllers and models
 # Link up comments to reviews routes
 # bookmark is singular as we can only bookmark one review per user
