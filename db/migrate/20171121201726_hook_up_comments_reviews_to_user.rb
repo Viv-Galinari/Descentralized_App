a848c476b2b20b596c3d84a4afe7718a94b07ddc
Viv <vivian_pr@hotmail.com>
2017-11-24 23:06:58 +0000

    sync files

--- db/migrate/20171121201726_hook_up_comments_reviews_to_user.rb
+++
@@ -1,8 +0,0 @@
-class HookUpCommentsReviewsToUser < ActiveRecord::Migration[5.1]
-  def change
-
-    add_column :reviews, :user_id, :integer
-    add_column :comments, :user_id, :integer
-
-  end
-end
